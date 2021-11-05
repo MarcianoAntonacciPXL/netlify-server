2604db9b10510b8942fd163a3b6b788edc89bff1
Marciano Antonacci <marcianoantonacci@MacBook-Air-van-Marciano.local>
2021-11-05 16:09:39 +0100

    added xhr

--- src/api.js
+++ src/api.js
@@ -2,10 +2,12 @@ const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const XMLHttpRequest = require('xhr2');
 
 const app = express();
 const router = express.Router();
-var jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
+const ZOHO_URL = 'https://flow.zoho.eu/20077367626/flow/webhook/incoming?zapikey=1001.b682318d11e63f88b1131a3ad1588c1c.a9a3821150f3abc1bf8409aeb1ce9231&isdebug=false';
 
 app.use(cors({
     origin: '*'
@@ -18,9 +20,23 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', jsonParser, (req, res) => {
-    body = JSON.stringify(req.body);
-    console.log(body);
-    res.send(body.toString());
+    const http = new XMLHttpRequest();
+    http.open("POST", ZOHO_URL, true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); 
+    http.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+    http.onreadystatechange = () => {
+        if (http.readyState == 4 && http.status == 200) {
+            res.statusCode = 200;
+            res.send("record created");
+        } else {
+            res.send("something went wrong");
+        }
+    }
+    
+    json = JSON.stringify(req.body);
+    http.send(json);   
 });
 
 app.use('/.netlify/functions/api',router);
