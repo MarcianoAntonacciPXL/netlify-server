921e20f6873cc5a135e2cfed4f491bca4ea74c51
Marciano Antonacci <marcianoantonacci@MacBook-Air-van-Marciano.local>
2021-11-05 15:58:58 +0100

    added bodyParser

--- src/api.js
+++ src/api.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const router = express.Router();
+var jsonParser = bodyParser.json();
 
 app.use(cors({
     origin: '*'
 }));
 
-router.get('/', (req, res) => {
+router.get('/', jsonParser, (req, res) => {
     res.json({
         'hello': 'hi'
     });
@@ -18,7 +20,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     body = JSON.stringify(req.body);
     console.log(body);
-    res.send("ok");
+    res.send(body.toString());
 });
 
 app.use('/.netlify/functions/api',router);
